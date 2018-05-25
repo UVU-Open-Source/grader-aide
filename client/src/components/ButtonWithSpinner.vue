aea1bf110a4bdabbff1612861110d55acab50ab5
Jcroxford <takeflightsc@gmail.com>
2018-05-24 23:21:27 -0600

    added reusable button with spinner using font awesome

---
+++ client/src/components/ButtonWithSpinner.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <button class="btn btn-success" disabled v-if="isLoaded">
+      <i class="fa fa-check"></i>
+      <slot name="success"></slot>
+    </button>
+    <button
+      class="btn btn-primary"
+      :disabled="isLoading"
+      @click="handleClick"
+      v-else
+    >
+      <i v-if="isLoading" class="fa fa-circle-o-notch fa-spin"></i>
+      <slot></slot>
+    </button>
+
+  </div>
+</template>
+
+<script>
+// @ts-nocheck
+// todo consider refactoring so that we don't need the whole font awesome library
+  export default {
+    name: 'vue-button-spinner',
+    props: {
+      isLoading: {
+        type: Boolean,
+        default: false
+      },
+      isLoaded: {
+        type: Boolean,
+        default: false
+      }
+    },
+    methods: {
+      handleClick() {
+        this.$emit('click')
+      }
+    }
+  }
+</script>
