e1edd3e14fd8fb782c33ad6998d6525095adde88
Jcroxford <takeflightsc@gmail.com>
2018-06-26 16:00:29 -0600

    added initial vuetify component modules to app

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,33 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import 'vuetify/dist/vuetify.min.css';
+
+import {
+  Vuetify,
+  VApp,
+  VBtn,
+  VIcon,
+  VGrid,
+  transitions,
+  VCard
+} from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+
+// @ts-ignore
 import App from './App'
 import router from './router'
+
+Vue.use(Vuetify, {
+  components: {
+    VApp,
+    VBtn,
+    VIcon,
+    VGrid,
+    transitions,
+    VCard
+  }
+})
 
 Vue.config.productionTip = false
 
