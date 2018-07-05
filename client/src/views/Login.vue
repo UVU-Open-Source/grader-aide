b29959f436b3ce4a2735af4f8e911a10791d3866
Jcroxford <takeflightsc@gmail.com>
2018-07-05 16:57:28 -0600

    added login vue with zybooks auth
    added login vue component and initizlied with vuex data and old zyboooks auth components. will eventually cover canvas as well but for now the entire zybooks work flow is covered

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,3 +1,53 @@
 <template>
-  <div>hello world</div>
+  <v-container>
+    <auth-success-card
+      v-if="zyToken"
+      success-message="Zybooks Token Received"
+    ></auth-success-card>
+    <zybooks-auth-card
+      v-else
+      :zyReqErr="zyReqErr"
+      @get-zybooks-token="getZybooksToken"
+    ></zybooks-auth-card>
+  </v-container>
 </template>
+
+<script>
+import { mapState } from 'vuex'
+// apis
+import * as zybooksApi from '@/apis/zybooks.api'
+import * as canvasApi from '@/apis/canvas.api'
+
+// components
+import ZybooksAuthCard from '@/components/ZybooksAuthCard'
+import CanvasAuthCard from '@/components/CanvasAuthCard'
+import AuthSuccessCard from '@/components/AuthSuccessCard'
+
+export default {
+  data() {
+    return {
+      // persistent
+      // fixme not used yet, these tokens
+      cToken: '',
+      // gui state
+      // fixme handle inside vuex?
+      cReqErr: ''
+    }
+  },
+  computed: mapState('auth', [
+    'zyToken',
+    'zyReqErr',
+    'zyAuthPending'
+  ]),
+  methods: {
+    getZybooksToken(authPayload) {
+      this.$store.dispatch('auth/loginZybooks', authPayload)
+    }
+  },
+  components: {
+    ZybooksAuthCard,
+    CanvasAuthCard,
+    AuthSuccessCard,
+  }
+}
+</script>
