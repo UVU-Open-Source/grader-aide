7b09e837699485d88895e02668a01a34695c7df4
Jcroxford <takeflightsc@gmail.com>
2018-07-05 16:55:37 -0600

    init auth module with full zybooks api integration

---
+++ client/src/store/modules/auth.js
@@ -0,0 +1,84 @@
+// apis
+import * as zybooksApi from '@/apis/zybooks.api'
+import * as canvasApi from '@/apis/canvas.api'
+
+
+// ================================================================================
+// initial state
+// ================================================================================
+const state = {
+  zyToken: '',
+  zyAuthPending: false,
+  zyReqErr: '',
+  cToken: '',
+  cReqErr: ''
+}
+
+// ================================================================================
+// getters
+// ================================================================================
+const getters = {
+
+}
+
+// ================================================================================
+// actions
+// ================================================================================
+const actions = {
+  // used to check auth status of zybooks when app is loaded initially
+  initZybooksAuth({ commit, dispatch }) {
+    zybooksApi
+      .getStoredZybooksAuthToken() // playload shape { token: string, error: string }
+      .then(response => dispatch('handleZybooksAuthResponse', response))
+      .catch(err => dispatch('handleZybooksAuthError', err))
+  },
+  // playload shape { zyEmail: string, zyPassword: string }
+  loginZybooks({ commit }, { zyEmail, zyPassword }) {
+    zybooksApi
+      .signin(zyEmail, zyPassword) // playload shape { token: string, error: string }
+      .then(response => commit('zybooksLoginSuccess', response))
+  },
+  // playload shape { token: string, error: string }
+  handleZybooksAuthResponse({ commit }, { token, error }) {
+    if(error) return commit('zybooksLoginFailed', error)
+
+    commit('zybooksLoginSuccess', token)
+  },
+  // payload expects an error object
+  handleZybooksAuthError({ commit }, err) {
+    // failing gracefully because if NO_TOKEN_STORED user needs to authenticate with zybooks
+    if(err.message === zybooksApi.NO_TOKEN_STORED) return
+
+    commit('zybooksLoginFailed', 'zybooks authentication failed due to internal bug')
+  }
+}
+
+// ================================================================================
+// mutations
+// ================================================================================
+const mutations = {
+  zybooksLogin() {
+    state.zyAuthPending = true
+  },
+  zybooksLoginSuccess(state, token) {
+    state.zyToken = token
+    state.zyReqErr = ''
+    state.zyAuthPending = false
+  },
+  zybooksLoginFailed(state, error) {
+    state.zyToken = ''
+    state.zyReqErr = error
+    state.zyAuthPending = false
+  }
+}
+
+// ================================================================================
+// exports
+// ================================================================================
+export default {
+  namespaced: true,
+  state,
+  actions,
+  mutations,
+  getters
+}
