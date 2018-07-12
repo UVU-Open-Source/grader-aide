e25df9d943e3873bdff5520d7661e5ade2cf38c7
Jcroxford <takeflightsc@gmail.com>
2018-07-12 16:09:21 -0600

    removed auth and temp disabled gradeZybooks view

--- client/src/views/GradeZybooks.vue
+++ client/src/views/GradeZybooks.vue
@@ -1,34 +1,7 @@
 <template>
+  <!-- // fixme needs to be refactored to use vuetify if this view continues to exist -->
   <div class="container">
-    <!-- zyboks auth -->
     <div class="row">
-      <div class="col-sm">
-        <auth-success-card
-          v-if="zyToken"
-          success-message="Zybooks Token Received"
-        ></auth-success-card>
-        <zybooks-auth-card
-          v-else
-          :zyReqErr="zyReqErr"
-          @get-zybooks-token="getZybooksToken"
-        ></zybooks-auth-card>
-      </div>
-
-      <!-- canvas auth -->
-      <div class="col-sm">
-        <auth-success-card
-          v-if="cToken"
-          success-message="Cavnas Token Received"
-        ></auth-success-card>
-        <canvas-auth-card
-          v-else
-          :cReqErr="cReqErr"
-          @save-canvas-token="saveCanvasToken"
-        ></canvas-auth-card>
-      </div>
-    </div>
-
-    <div class="row" v-if="authTokensReceived">
       <div class="col-sm-2 mt-5" v-for="chapter of chapters" :key="chapter.value">
         <button-with-spinner
           :isLoading="chapter.loading"
@@ -52,20 +25,14 @@
 import axios from 'axios'
 import BASE_URL from '@/utils/baseURL'
 
-// apis
-import * as zybooksApi from '@/apis/zybooks.api'
-import * as canvasApi from '@/apis/canvas.api'
-
 // components
-import ZybooksAuthCard from '@/components/ZybooksAuthCard'
-import CanvasAuthCard from '@/components/CanvasAuthCard'
-import AuthSuccessCard from '@/components/AuthSuccessCard'
 import ButtonWithSpinner from '@/components/ButtonWithSpinner'
 
 export default {
   data() {
     return {
       // persistent
+      // fixme needs tokens to be usable again.
       zyToken: '',
       cToken: '',
       // gui state
@@ -126,84 +93,32 @@ export default {
       ]
     }
   },
-  computed: {
-    authTokensReceived() {
-      return this.zyToken && this.cToken
-    }
-  },
   methods: {
-    getZybooksToken(credentials) {
-      // todo needs real error handling done in some capacity
-      const { zyEmail, zyPassword } = credentials
-
-      zybooksApi
-        .signin(zyEmail, zyPassword)
-        .then(this.handleUpdateZybooksAuthValues)
-    },
-    handleUpdateZybooksAuthValues({ token, error }) {
-      if(error) return this.zyReqErr = error
-
-      this.zyToken = token
-      this.zyReqErr = ''
-    },
-    handleUpdateCanvasAuthValues({ token, error }) {
-      if(error) return this.cReqErr = error
-
-      this.cToken = token
-      this.cReqErr = ''
-    },
-    saveCanvasToken(token) {
-      // todo add error modal or somethign?
-      canvasApi
-        .saveToken(token)
-        .then(this.handleUpdateCanvasAuthValues)
-    },
     SubmitGradesFor(chapter) {
+      // fixme disabled submitting grades for now since class is inactice. 
       chapter.loading = true
+      setTimeout(() => {
+        chapter.loading = false
+        chapter.loaded = true
+      }, 1000);
 
-      const config = {
-        headers: {
-          zyToken: this.zyToken,
-          cToken: this.cToken
-        }
-      }
+      // const config = {
+      //   headers: {
+      //     zyToken: this.zyToken,
+      //     cToken: this.cToken
+      //   }
+      // }
 
-      axios
-        .get(`${BASE_URL}/api/v1/grade/zybooks/chapter/${chapter.value}`, config)
-        .then(response => {
-          chapter.loading = false
-          if(response.data.success) chapter.loaded = true
-        })
-        .catch(e => chapter.loading = false)
+      // axios
+      //   .get(`${BASE_URL}/api/v1/grade/zybooks/chapter/${chapter.value}`, config)
+      //   .then(response => {
+      //     chapter.loading = false
+      //     if(response.data.success) chapter.loaded = true
+      //   })
+      //   .catch(e => chapter.loading = false)
     }
-  },
-  created() {
-    const component = this
-
-    zybooksApi
-      .getStoredZybooksAuthToken()
-      .then(this.handleUpdateZybooksAuthValues)
-      .catch(err => {
-        // todo needs full error handling implemented for now just console logging unhandled error
-        if(err.message !== zybooksApi.NO_TOKEN_STORED) console.log(err)
-
-        // failing gracefully because if NO_TOKEN_STORED user needs to authenticate with zybooks
-      })
-
-    canvasApi
-      .getStoredCanvasAuthToken()
-      .then(this.handleUpdateCanvasAuthValues)
-      .catch(err => {
-        // todo needs full error handling implemented for now just console logging unhandled error
-        if(err.message !== canvasApi.NO_TOKEN_STORED) console.log(err)
-
-        // failing gracefully because if NO_TOKEN_STORED user needs to authenticate with zybooks
-      })
   },
   components: {
-    ZybooksAuthCard,
-    CanvasAuthCard,
-    AuthSuccessCard,
     ButtonWithSpinner
   }
 }
