14b9cfc66a0b90b1c5434f5f65ce1776c267d452
Jcroxford <takeflightsc@gmail.com>
2018-08-02 18:17:04 -0600

    added init prototyping for course views

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -3,6 +3,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import GradeZybooks from '@/views/GradeZybooks'
 import Login from '@/views/Login'
+import CourseList from '@/views/CourseList'
+import EditCourse from '@/views/EditCourse'
 
 Vue.use(Router)
 
@@ -12,6 +14,16 @@ export default new Router({
       path: '/',
       name: 'root',
       component: GradeZybooks
+    },
+    {
+      path: '/courses',
+      name: 'courses',
+      component: CourseList
+    },
+    {
+      path: '/courses/:courseId/edit',
+      name: 'edit-course',
+      component: EditCourse
     },
     {
       path: '/login',
---
+++ client/src/views/CourseList.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-container grid-list-md>
+    <v-layout row wrap>
+      <v-flex xs12 sm6 v-for="course of courses" :key="course.id">
+        <v-card>
+          <v-card-text>
+            <h3 class="headline mb-0">{{course.name}}</h3>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn flat color="green">
+              Assignments
+              <v-icon class="ml-2">list</v-icon>
+            </v-btn>
+            <v-btn flat color="orange lighten-2">
+              Edit
+              <v-icon class="ml-2">edit</v-icon>
+            </v-btn>
+            <v-btn flat color="red lighten-2" vif="!course.registered">
+              Register
+              <v-icon class="ml-2">edit</v-icon>
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      courses: [
+        {
+          "id": "466507",
+          "name": "CS-2550-001 | Summer 2018 B1",
+          registered: true
+        },
+        {
+          "id": "466508",
+          "name": "CS-2550-001 | Summer 2018 B1",
+          registered: true
+        },
+        {
+          "id": "466509",
+          "name": "CS-2550-001 | Summer 2018 B1",
+          registered: false
+        }
+      ]
+    }
+  }
+}
+</script>
