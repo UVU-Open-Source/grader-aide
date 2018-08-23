ce890d05152c7ac72d3330a13aed568e53974a16
Jcroxford <takeflightsc@gmail.com>
2018-08-23 16:33:02 -0600

    initialized knex for postgres connections

---
+++ server/knexfile.js
@@ -0,0 +1,32 @@
+module.exports = {
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/grader_aide_test',
+    migrations: {
+      directory: __dirname + '/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds/test'
+    }
+  },
+  dev: {
+    client: 'pg',
+    connection: 'postgres://localhost/grader_aide',
+    migrations: {
+      directory: __dirname + '/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds/development'
+    }
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: __dirname + '/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds/production'
+    }
+  }
+};
