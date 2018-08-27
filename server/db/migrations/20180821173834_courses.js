02aef00c5ecb22c5bac69221951b9a75390293a8
Jcroxford <takeflightsc@gmail.com>
2018-08-27 15:13:30 -0600

    added unique canvas id constraint to courses table

--- server/db/migrations/20180821173834_courses.js
+++ server/db/migrations/20180821173834_courses.js
@@ -2,7 +2,7 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('courses', (table) => {
     table.increments()
-    table.string('canvasId').notNullable()
+    table.string('canvasId').notNullable().unique()
     table.string('name').notNullable()
     table.string('zyLink').defaultTo('')
   })
