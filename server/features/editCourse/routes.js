82778a2bc7c393d50e72474b4caaf7829e7f977e
Jcroxford <takeflightsc@gmail.com>
2018-08-27 20:36:22 -0600

    added primitive error handling to edit course routes

--- server/features/editCourse/routes.js
+++ server/features/editCourse/routes.js
@@ -4,9 +4,10 @@ var removeDiacritics = require('diacritics').remove;
 
 const queries = require('./queries')
 const canvasApi = require('../../utils/canvas.api')
+const zybooksApi = require('../../utils/zybooks.api')
 
 // query canvas for a course
-router.get('/canvas/course/:courseId', (req, res) => {
+router.get('/canvas/courses/:courseId', (req, res) => {
   const cToken = req.get('cToken')
   const { courseId } = req.params
 
@@ -17,13 +18,17 @@ router.get('/canvas/course/:courseId', (req, res) => {
     })
 })
 
-router.post('/course/register', (req, res) => {
+router.post('/courses/register', (req, res) => {
   const { students, ...course } = req.body
 
   // zyLink required to before you can register class
   if (!course.zyLink) res.status(400).json({ error: `zybooks is required to register a class` })
 
-  queries.addCourse(course).then(res.json.bind(res))
+  queries.addCourse(course)
+    .then(res.json.bind(res))
+    .catch(e => {
+      res.status(500).end()
+    })
 })
 
 router.get('/courses/unregistered', (req, res) => {
@@ -40,6 +45,9 @@ router.get('/courses/unregistered', (req, res) => {
     })
     .then(([active, registered]) => R.differenceWith((a, r) => a.id === r.canvasId, active, registered))
     .then(res.json.bind(res))
+    .catch(e => {
+      res.status(500).end()
+    })
 })
 
 router.get('/courses/registered', (req, res) => {
@@ -52,6 +60,9 @@ router.get('/courses/registered', (req, res) => {
       return queries.findRegisteredCourses(cIds)
     })
     .then(res.json.bind(res))
+    .catch(e => {
+      res.status(500).end()
+    })
 })
 
 module.exports = router
