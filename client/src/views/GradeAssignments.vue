5c05df5cfc6713a3caa0a8cf8818720f1358ba89
Jcroxford <takeflightsc@gmail.com>
2018-08-27 22:33:47 -0600

    refactored grade assigments to use vuetify and custom rest endpoints
    component now more future proof for adding future assignment types and also interfaces with custom microservice to get chapter data for grading and submitting grades

---
+++ client/src/views/GradeAssignments.vue
@@ -0,0 +1,124 @@
+<template>
+  <v-container grid-list-md>
+    <v-layout row wrap>
+      <v-flex xs12 v-if="course && chapters.length">
+        <v-card>
+          <v-card-title primary-title class="ml-1">
+            <h3 class="headline mb-0">{{course.name}}</h3>
+          </v-card-title>
+
+          <v-card-text>
+            <!-- list zybooks chapter assignments -->
+            <v-layout row wrap>
+              <v-flex xs12 class="title ml-2">
+                Grade Zybooks chapters
+              </v-flex>
+
+              <v-flex xs2 v-for="chapter of chapters" :key="chapter.value">
+                <button-with-spinner
+                  :isLoading="chapter.loading"
+                  :isLoaded="chapter.loaded"
+                  :disabled="!chapter.gradeable"
+                  @click="SubmitGradesFor(chapter)"
+                >
+                  Grade Chapter {{ chapter.value }}
+
+                  <template slot="success" v-if="chapter.loaded">
+                    <span>Grade Submitted</span>
+                  </template>
+                </button-with-spinner>
+              </v-flex>
+            </v-layout>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+
+      <!-- failed to load ajaxx data -->
+      <v-flex xs4 offset-xs4 v-if="pageLoadErr">
+        <v-card>
+          <v-card-text>
+            <p class="red--text text-sm-center">{{pageLoadErr}}</p>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+
+      <!-- loading icon -->
+      <v-flex xs2 offset-xs5 class="text-sm-center" v-if="loading">
+        <v-progress-circular
+          :size="75"
+          color="success"
+          indeterminate
+        ></v-progress-circular>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+import * as moment from 'moment'
+
+import axiosWithAuthHeaders from '../mixins/axiosWithAuthHeaders'
+import apiUtils from '../utils/apiUtils'
+
+// components
+import ButtonWithSpinner from '@/components/ButtonWithSpinner'
+
+export default {
+  mixins: [ axiosWithAuthHeaders ],
+  data() {
+    return {
+      // persistent
+      course: null,
+      // gui state
+      chapters: [],
+      pageLoadErr: '',
+      loading: true
+    }
+  },
+  methods: {
+    SubmitGradesFor(chapter) {
+      // fixme disabled submitting grades for now since class is inactice.
+      chapter.loading = true
+      setTimeout(() => {
+        chapter.loading = false
+        chapter.loaded = true
+      }, 1000);
+
+      // this.authAxios
+      //   .get(`/api/v1/grade/zybooks/chapter/${chapter.value}`)
+      //   .then(response => {
+      //     chapter.loading = false
+      //     if(response.data.success) chapter.loaded = true
+      //   })
+      //   .catch(e => chapter.loading = false)
+    },
+    createChapterStateObj(chapter) {
+      return {
+        value: parseInt(chapter.name.replace('ch', '')),
+        loading: false,
+        loaded: false,
+        gradeable: moment().diff(chapter.due_at, 'seconds') > 0
+      }
+    }
+  },
+  created() {
+    const canvasCourseId = this.$route.params.canvasCourseId
+    Promise.all([
+      this.authAxios.get(`/api/v1/canvas/courses/${canvasCourseId}`).then(apiUtils.pluckData),
+      this.authAxios.get(`/api/v1/courses/${canvasCourseId}/assignments`).then(apiUtils.pluckData)
+    ])
+      .then(([course, assignments]) => {
+        this.loading = false
+        this.course = course
+        this.chapters = assignments.map(this.createChapterStateObj)
+      })
+      .catch(err => {
+        this.loading = false
+        this.pageLoadErr = 'Unable to load course or assignments'
+      })
+  },
+  components: {
+    ButtonWithSpinner
+  }
+}
+</script>
